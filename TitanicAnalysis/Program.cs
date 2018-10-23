7a3004ad42636608d4b13e2009311d3ceb4a193c
elizabeth hahn <kendoka69@mac.com>
2018-10-23 15:44:55 -0400

    Initial commit.  Added data file.  Added StreamReader to read file

--- TitanicAnalysis/Program.cs
+++ TitanicAnalysis/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TitanicAnalysis
 {
@@ -10,6 +11,35 @@ namespace TitanicAnalysis
     {
         static void Main(string[] args)
         {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "TitanicData.csv");
+            var fileContents = ReadTitanicData(fileName);
         }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static List<string[]> ReadTitanicData(string fileName)
+        {
+            var titanicData = new List<string[]>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    titanicData.Add(values);
+
+                }
+            }
+            return titanicData;
+        }
+
     }
 }
